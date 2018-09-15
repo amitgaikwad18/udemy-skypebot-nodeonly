e4a4cde0742637b34c70ad00260b34e29ce7ebd0
Amit Gaikwad <amitgaikwad85@gmail.com>
2018-09-15 21:27:23 +0530

    added console log

--- app.js
+++ app.js
@@ -1,51 +1,38 @@
+//import { isNumber } from 'util';
+
 //calling microsoft botbuilder
-import { MemoryBotStorage, ChatConnector, UniversalBot } from 'botbuilder';
+var botbuilder = require('botbuilder');
 
 // setting up InMemoryStorage
-var inMemoryStorage = new MemoryBotStorage();
+var inMemoryStorage = new botbuilder.MemoryBotStorage();
 
 //calling express
-import express from 'express'; 
+var express = require('express'); 
 
 var app = express();
 
 var listenPort = process.env.PORT || 3000;
 
 // Connect to microsoft bot service
-var botConnector = new ChatConnector(
+var botConnector = new botbuilder.ChatConnector(
     {
         appId: '05af2f2c-a366-4e99-94a8-647d988335f6',
         appPassword: 'shhqgPHHC83{psGDW123(:*'
     });
 
 // Bot Service listener
-var bot = new UniversalBot(botConnector).set('storage',inMemoryStorage);
+var bot = new botbuilder.UniversalBot(botConnector).set('storage',inMemoryStorage);
 
 //connect botservice to app
 app.post('/botservice/msg', botConnector.listen());
-
-
-
-bot.dialog('/', function(session)
-{
-    // var skypeMessage = session.message.text.toLowerCase();
-    // if(skypeMessage === 'hi' || skypeMessage === 'hello' || skypeMessage === 'hey')
-    // {
-    //     session.send('Hello there, I am your Currency converter bot. Let\'s talk money');
-    // }
-    // else{
-    //     session.send('Me no understand');
-    // }
-    CurrencyConvertor(session);
-});
 
 CurrencyConvertor = function (session) {
 
     userMsg = session.message.text;
     var msg;
 
-    console.log('userMsg >>> '+userMsg);
-    
+    console.log('user message >>> '+userMsg);
+
     var currencyObtained = false;
     var countryObtained = false;
     var dollarValue = 0.0;
@@ -78,9 +65,26 @@ CurrencyConvertor = function (session) {
     }
 }
 
+ConverCurrency == function(session){
+    
+}
+
+bot.dialog('/', function(session)
+{
+
+    // var skypeMessage = session.message.text.toLowerCase();
+    // if(skypeMessage === 'hi' || skypeMessage === 'hello' || skypeMessage === 'hey')
+    // {
+    //     session.send('Hello there, I am your Currency converter bot. Let\'s talk money');
+    // }
+    // else{
+    //     session.send('Me no understand');
+    // }
+    CurrencyConvertor(session);
+});
+
 app.get('/', function(req, res){
     res.send('Currency Converter Bot Listening...');
-    
 })
 
 app.listen(listenPort, function(){
