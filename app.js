573f98520674b3b264fed1ef21d1f37b3d8de9df
Amit Gaikwad <amitgaikwad85@gmail.com>
2018-09-15 21:24:41 +0530

    added console log

--- app.js
+++ app.js
@@ -1,49 +1,63 @@
-//import { isNumber } from 'util';
-
 //calling microsoft botbuilder
-var botbuilder = require('botbuilder');
-var isNumber = require('util');
+import { MemoryBotStorage, ChatConnector, UniversalBot } from 'botbuilder';
 
 // setting up InMemoryStorage
-var inMemoryStorage = new botbuilder.MemoryBotStorage();
+var inMemoryStorage = new MemoryBotStorage();
 
 //calling express
-var express = require('express'); 
+import express from 'express'; 
 
 var app = express();
 
 var listenPort = process.env.PORT || 3000;
 
 // Connect to microsoft bot service
-var botConnector = new botbuilder.ChatConnector(
+var botConnector = new ChatConnector(
     {
         appId: '05af2f2c-a366-4e99-94a8-647d988335f6',
         appPassword: 'shhqgPHHC83{psGDW123(:*'
     });
 
 // Bot Service listener
-var bot = new botbuilder.UniversalBot(botConnector).set('storage',inMemoryStorage);
+var bot = new UniversalBot(botConnector).set('storage',inMemoryStorage);
 
 //connect botservice to app
 app.post('/botservice/msg', botConnector.listen());
 
-CurrencyConvertor = function (session){
+
+
+bot.dialog('/', function(session)
+{
+    // var skypeMessage = session.message.text.toLowerCase();
+    // if(skypeMessage === 'hi' || skypeMessage === 'hello' || skypeMessage === 'hey')
+    // {
+    //     session.send('Hello there, I am your Currency converter bot. Let\'s talk money');
+    // }
+    // else{
+    //     session.send('Me no understand');
+    // }
+    CurrencyConvertor(session);
+});
+
+CurrencyConvertor = function (session) {
 
     userMsg = session.message.text;
     var msg;
 
+    console.log('userMsg >>> '+userMsg);
+    
     var currencyObtained = false;
     var countryObtained = false;
     var dollarValue = 0.0;
     var country;
 
-    userName = session.message.user.name;
+    // userName = session.message.user.name;
 
     if(typeof userMsg === 'number'){
         msg = userMsg;
     }
     else {
-        msg = userMsg.toLowerCase;
+        msg = userMsg.toLowerCase();
     }
 
     if(msg === 'hi' || msg === 'hello' || msg === 'hey')
@@ -64,22 +78,9 @@ CurrencyConvertor = function (session){
     }
 }
 
-
-bot.dialog('/', function(session)
-{
-    // var skypeMessage = session.message.text.toLowerCase();
-    // if(skypeMessage === 'hi' || skypeMessage === 'hello' || skypeMessage === 'hey')
-    // {
-    //     session.send('Hello there, I am your Currency converter bot. Let\'s talk money');
-    // }
-    // else{
-    //     session.send('Me no understand');
-    // }
-    CurrencyConvertor(session);
-});
-
 app.get('/', function(req, res){
     res.send('Currency Converter Bot Listening...');
+    
 })
 
 app.listen(listenPort, function(){
